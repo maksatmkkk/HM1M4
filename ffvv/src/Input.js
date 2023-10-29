99246b90c54e80104ca988a4d19e205b55287a3e
maksatmkkk <143606944+maksatmkkk@users.noreply.github.com>
2023-10-29 21:39:13 +0600

    Add files via upload

---
+++ ffvv/src/Input.js
@@ -0,0 +1,94 @@
+import React from 'react'
+import './Style.css'
+
+
+const Input = () => {
+  return (
+    <div className='Body'>
+        
+        <div className='header'>
+        <div className='logoFrame'></div>
+        <div className='Btns'>
+          <p>Home</p>
+        <p>How it works</p>
+        <p>What we do</p>
+        <p>Resources</p>
+        <button className='Login'>Login</button>
+        <button className='Talk-to-an-Expert'>Talk to an Expert</button>
+        </div>
+    </div>
+
+    <div className='Finansed'>
+   <div className='Text'>
+    <p className='Baner'>Get your finances right</p>
+    <p className='About'>Get your finances right with Accountancy Cloud. We offer the best accounting, R&D credits and CFO services for ambitious businesses who want to grow.</p>
+    <p className='Switch'>Switching to us is simple</p>
+    <button className='Talk-Fin'>Talk to us</button>
+   </div>
+    
+    <div className='ImgPc'></div>
+
+    </div>
+
+    <div className='Best'>
+
+      <div className='ImgBest'></div>
+
+     <div className='Text'>
+      <p className='BestText'>The best finance stack for entrepreneurs </p>
+      <p className='BestText1'>70% of companies switch to us from their DIY or legacy system. Now, get your bookkeeping, tax prep and CFO problems, solved. Complete our contact form, and find out why 9 out of 10 customers recommend us.</p>
+     </div>
+    </div>
+
+    <div className='WhyCon'>
+      <div className='Why'>
+      <p  className='BestText'>Why Switch?</p>
+      <div className='ImgWhy'></div>
+      <button className='WhyDis'>Discover what we do</button>
+    </div>
+    </div>
+
+    <div className='Pink'>
+
+      <div className='PlansCon'>
+      <p className='Plans'>Plans built for growth</p>
+      <div className='ImgPink'></div>
+      </div>
+    </div>
+
+    <div className='Hub'>
+      <div className='TextHub'>
+        <p className='Ac'>The AC Hub</p>
+        <p className='Ex'>Explore our dedicated online resources. We've got the tips, tricks and online accounting know-how for business success.</p>
+      <button className='HubBtn'>View Hub</button>
+      </div>
+      <div className='HubImg'></div>
+      
+    </div>
+
+
+    <div className='ReadyCon'>
+     <div className='Ready'>
+      <div className='TextRed'>
+        <p className='SwitchRed'>Ready to Switch? It's simple.</p>
+        <p className='SpeakRed'>Speak to an expert today</p>
+      </div>
+      <button className='GetRed'>Get Started</button>
+
+     </div>
+
+    </div>
+    <div className='Footer'></div>
+
+
+
+
+
+
+
+
+
+    </div>)
+}
+
+export default Input
